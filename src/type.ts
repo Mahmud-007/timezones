614e2a73b207709a5db6dbe15a96e25718487e38
Mahmud-007 <mahmud6799@gmail.com>
2022-12-06 19:44:51 +0600

    first commit

---
+++ src/type.ts
@@ -0,0 +1,15 @@
+export type User = {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    role: string;
+}
+export type Record = {
+    id: string;
+    name: string;
+    city: string;
+    timezone: string;
+    diff_gmt: string;
+    creator: string;
+}
