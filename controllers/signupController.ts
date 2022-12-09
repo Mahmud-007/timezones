27a96a3b4cc77c61b302ebadffb7d96da5c38652
Mahmud-007 <mahmud6799@gmail.com>
2022-12-09 12:31:50 +0600

    add user signup

---
+++ controllers/signupController.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import User from "../models/user";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+type Data = {
+  name: string;
+};
+
+export default async function signupController(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { name, email, role, password } = req.body;
+    if (!(email && password && name)) {
+      res.status(400).send("All input is required");
+    }
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+      role,
+    });
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY || "",
+      {
+        expiresIn: "2h",
+      }
+    );
+    user.token = token;
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+}
---
+++ pages/api/signup.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import connectDB from "../../utils/connectDB";
+import signupController from "../../controllers/signupController";
+
+type Data = {
+  name: string;
+};
+
+export default async function signup(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  console.log(req.body);
+  try {
+    await connectDB();
+    signupController(req, res);
+  } catch (err) {
+    console.log({ err });
+  }
+}
