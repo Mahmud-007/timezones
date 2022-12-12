4a964b774a143692b6c137fcc0251685e439766f
Mahmud-007 <mahmud6799@gmail.com>
2022-12-12 13:30:37 +0600

    update records

--- pages/api/records.ts
+++ pages/api/records.ts
@@ -13,16 +13,9 @@ export default async function records(
     await connectDB();
     const user = await isAuth(token.split(" ")[1]);
     console.log({ user });
-    if (user.role === "admin") {
-      const records = await Record.find({});
-      console.log({ records });
-      res.status(200).json({records});
-    }
-    else{
-        const records = await Record.find({user:user._id});
-        console.log({ records });
-        res.status(200).json({records});
-    }
+    const records = await Record.find({});
+    console.log({ records });
+    res.status(200).json({ records });
   } catch (err) {
     console.log(err);
   }
